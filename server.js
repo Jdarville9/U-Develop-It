cb16624c2b0f5a64da22949b14ad8ecb1763e6fb
Joseph Darville <joseph.darville9@gmail.com>
2022-07-08 16:34:29 -0400

    created function that allow us to use SQL through JS

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -5,14 +6,32 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+//Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //your mysql username
+        user: 'root',
+        //your MySQL password
+        password: 'Capital9!',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
 app.get('/', (req, res) => {
     res.json({
         message: 'Hello World'
     });
 });
 
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
+
 // must be placed after all get routes, otherwise it'll override other routes
 // default response for any other request (Not Found)
+// it's a catchall
 app.use((req, res) => {
     res.status(404).end();
 });
