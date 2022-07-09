5c0be6c3b96eb06bb848eb3ec1c11bc9a2c9b343
Joseph Darville <joseph.darville9@gmail.com>
2022-07-08 20:25:46 -0400

    joined tables

--- server.js
+++ server.js
@@ -28,7 +28,11 @@ app.get('/', (req, res) => {
 
 //GET ALL CANDIDATES
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates.*, parties.name 
+    AS party_name 
+    FROM candidates 
+    LEFT JOIN parties 
+    ON candidates.party_id = parties.id`;
 
     db.query(sql, (err, rows) => {
         if (err) {
@@ -43,8 +47,13 @@ app.get('/api/candidates', (req, res) => {
 });
 
 // GET SINGLE CANDIDATE
-app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates WHERE id = ?`;
+app.get('/api/candidates/:id', (req, res) => {
+    const sql = `SELECT candidates.*, parties.name 
+    AS party_name 
+    FROM candidates 
+    LEFT JOIN parties 
+    ON candidates.party_id = parties.id 
+    WHERE candidates.id = ?`;
     const params = [req.params.id];
   
     db.query(sql, params, (err, row) => {
