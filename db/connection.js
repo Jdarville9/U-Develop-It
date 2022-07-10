62cafb7fb2428d98176113aa534b272d69c14105
Joseph Darville <joseph.darville9@gmail.com>
2022-07-09 21:59:56 -0400

    added routes and endpoints for voters table

---
+++ db/connection.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql2');
+
+//Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //your mysql username
+        user: 'root',
+        //your MySQL password
+        password: 'Capital9!',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+module.exports = db;
