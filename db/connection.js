def9222bca0ed8d0f265907e44ca79fdb0733da9
Joseph Darville <joseph.darville9@gmail.com>
2022-07-13 20:00:37 -0400

    necessary changes

--- db/connection.js
+++ db/connection.js
@@ -1,16 +1,16 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 //Connect to database
 const db = mysql.createConnection(
-    {
-        host: 'localhost',
-        //your mysql username
-        user: 'root',
-        //your MySQL password
-        password: 'Capital9!',
-        database: 'election'
-    },
-    console.log('Connected to the election database.')
+  {
+    host: "localhost",
+    //your mysql username
+    user: "root",
+    //your MySQL password
+    password: "CrazyFish33!",
+    database: "election",
+  },
+  console.log("Connected to the election database.")
 );
 
 module.exports = db;
